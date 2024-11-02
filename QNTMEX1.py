9e45021ee3dad92737078172abf74451e28c8966
arnavkalotra <arnavkalotra@gmail.com>
2024-11-02 15:20:25 -0400

    Add files via upload
    Various testings on quantum circuits
    ibm labeled circuits were tested on ibm servers, the rest were on the AER simulator

---
+++ QNTMEX1.py
@@ -0,0 +1,5 @@
+from qiskit_ibm_runtime import QiskitRuntimeService
+
+service = QiskitRuntimeService(channel="ibm_quantum", token="3b440d0fe3c4010c7acb05061ac8b94a63df503a0dafdb7ab83d1a6001a0fe00c7f74e176b703105d58ade63dc71ddf7548ab30b17aea22c576c2dc8a7d6a2e6")
+
+
